cc0aa8974adb4744e1d655cfcbc5d16af45d13ce
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-18 14:42:44 +0600

    modified display load issues

--- script/home.js
+++ script/home.js
@@ -1,3 +1,10 @@
+const createElement = (array) => {
+    const htmlElement = array.map (el => {
+        return `<span class="rounded-3xl bg-[#FEECEC] text-sm text-[#EF4444] font-medium border border-[#FECACA] px-4 py-1"><i class="fa-solid fa-bug"></i>${el}</span>`;
+    })
+    return htmlElement.join (' ');
+}
+
 // load-Issues
 const loadIssues = () => {
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
@@ -6,37 +13,58 @@ const loadIssues = () => {
     .then (data => displayLoadIssues (data.data))
 }
 
+// {
+//     "id": 1,
+//     "title": "Fix navigation menu on mobile devices",
+//     "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+//     "status": "open",
+//     "labels": [
+//         "bug",
+//         "help wanted"
+//     ],
+//     "priority": "high",
+//     "author": "john_doe",
+//     "assignee": "jane_smith",
+//     "createdAt": "2024-01-15T10:30:00Z",
+//     "updatedAt": "2024-01-15T10:30:00Z"
+// }
+
 // display load-issue
 const displayLoadIssues = (issues) => {
+    console.log(issues);
+    
     const issueCardContainer = document.getElementById ('issueCard-container');
-    // issueCardContainer.innerHTML = '';
+    issueCardContainer.innerHTML = '';
 
     issues.forEach (issue => {
+        console.log(issue);
+        
         const issueCard = document.createElement ('div');
         issueCard.className = 'rounded-md shadow-md border-t-4 border-t-[#00A96E] bg-white';
         issueCard.innerHTML = `
         <div class="p-6 space-y-5">
                 <div class="flex justify-between items-center">
-                    <img src="./assets/Open-Status.png" alt="">
-                    <p class="text-sm text-[#EF4444] font-medium bg-[#FEECEC] rounded-full px-10 py-2">HIGH</p>
+                    <div>
+                    ${issue.status === 'open' 
+                        ?`<img src="./assets/Open-Status.png" alt=""/>` 
+                        :`<img src="./assets/Closed- Status .png" alt=""/>`}
+                    </div>
+                    <p class="text-sm text-[#EF4444] font-medium bg-[#FEECEC] rounded-full px-10 py-2">${issue.priority}</p>
                 </div>
 
                 <div>
-                    <h2 class="text-lg font-semibold text-[#1F2937] mb-2">Fix navigation menu on mobile devices</h2>
-                    <p class="text-sm text-[#64748B]">The navigation menu doesn't collapse properly on mobile devices...</p>
+                    <h2 class="text-[16px] font-semibold text-[#1F2937] mb-2">${issue.title}</h2>
+                    <p class="text-[13px] text-[#64748B]">${issue.description}</p>
                 </div>
 
-                <div class="space-x-1">
-                    <span class="rounded-3xl bg-[#FEECEC] text-sm text-[#EF4444] font-medium border border-[#FECACA] px-4 py-1"><i class="fa-solid fa-bug"></i> BUG</span>
-                    <span class="rounded-3xl bg-[#FFF8DB] text-sm text-[#D97706] font-medium border border-[#FDE68A] px-4 py-1"><i class="fa-solid fa-life-ring"></i> HELP WANTED</span>
-                </div>
+                <div class="space-x-1">${createElement (issue.labels)}</div>
             </div>
 
             <hr class="border border-gray-200">
 
             <div class="p-6 space-y-2">
-                <p class="text-[#64748B]">#1 by john_doe</p>
-                <p class="text-[#64748B]">1/15/2024</p>
+                <p class="text-[#64748B]">#${issue.id} ${issue.author}</p>
+                <p class="text-[#64748B]">${issue.createdAt}</p>
             </div>
         `;
         issueCardContainer.appendChild (issueCard);
