6089359f59f31fbf55b5f446b17b5b31d4132cbb
Masuda Akter Tisha <masudatisha63@gmail.com>
2026-05-19 09:42:47 +0600

    designed open and closed issue card border dynamically

--- script/home.js
+++ script/home.js
@@ -1,3 +1,4 @@
+
 const createElement = (array) => {
     const htmlElement = array.map (el => {
         return `<span class="rounded-3xl bg-[#FEECEC] text-sm text-[#EF4444] font-medium border border-[#FECACA] px-4 py-1"><i class="fa-solid fa-bug"></i>${el}</span>`;
@@ -13,22 +14,6 @@ const loadIssues = () => {
     .then (data => displayLoadIssues (data.data))
 }
 
-// {
-//     "id": 1,
-//     "title": "Fix navigation menu on mobile devices",
-//     "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
-//     "status": "open",
-//     "labels": [
-//         "bug",
-//         "help wanted"
-//     ],
-//     "priority": "high",
-//     "author": "john_doe",
-//     "assignee": "jane_smith",
-//     "createdAt": "2024-01-15T10:30:00Z",
-//     "updatedAt": "2024-01-15T10:30:00Z"
-// }
-
 // display load-issue
 const displayLoadIssues = (issues) => {
     console.log(issues);
@@ -40,7 +25,13 @@ const displayLoadIssues = (issues) => {
         console.log(issue);
         
         const issueCard = document.createElement ('div');
-        issueCard.className = 'rounded-md shadow-md border-t-4 border-t-[#00A96E] bg-white';
+
+        let borderColor = 'border-t-4 border-[#00A96E]';
+        if (issue.status === 'closed') {
+             borderColor = 'border-t-4 border-[#A855F7]';
+        }
+       
+        issueCard.className = `rounded-md shadow-md bg-white ${borderColor}`;
         issueCard.innerHTML = `
         <div class="p-6 space-y-5">
                 <div class="flex justify-between items-center">
@@ -49,8 +40,8 @@ const displayLoadIssues = (issues) => {
                         ?`<img src="./assets/Open-Status.png" alt=""/>` 
                         :`<img src="./assets/Closed- Status .png" alt=""/>`}
                     </div>
-                    <p class="text-sm text-[#EF4444] font-medium bg-[#FEECEC] rounded-full px-10 py-2">${issue.priority}</p>
-                </div>
+                    <p class="text-sm font-medium rounded-full px-10 py-2 uppercase text-[#EF4444] bg-[#FEECEC]">${issue.priority}</p>
+                 </div>
 
                 <div>
                     <h2 class="text-[16px] font-semibold text-[#1F2937] mb-2">${issue.title}</h2>
